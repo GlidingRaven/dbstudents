1237d56b7fddd8f7009d45afbb1a7571ba90fbe5
Dunaevsky <rainbowdash2016@yandex.ru>
2016-07-16 17:24:00 +0600

    secure #3

--- mine.js
+++ mine.js
@@ -11,7 +11,7 @@ function forsource(){
   var date_month = $('#date_month').val();
   var date_year = $('#date_year').val();
   
-  $.post("http://ravengo.ru/forspec/addersource.php", { bigtext: bigtext, name_uz: name_uz, date_day: date_day, date_month: date_month, date_year: date_year, url_source: url_source } )
+  $.post("addersource.php", { bigtext: bigtext, name_uz: name_uz, date_day: date_day, date_month: date_month, date_year: date_year, url_source: url_source } )
     .done(function(data) {alert("Data Loaded: " + data);$("#bigtext").val("");$("#url_source").val("");})
     .fail(function() {
     alert("fail");
@@ -24,7 +24,7 @@ function foruz(){
   var abb_name_uz = $('#abb_name_uz').val();
   var url_site = $('#url_site').val();
   //alert("syka");
-  $.post("http://ravengo.ru/forspec/adderuz.php", { city_code: city_code, full_name_uz: full_name_uz, abb_name_uz: abb_name_uz, url_site: url_site } )
+  $.post("adderuz.php", { city_code: city_code, full_name_uz: full_name_uz, abb_name_uz: abb_name_uz, url_site: url_site } )
     .done(function(data) {
         if(Number(data)==200){
                 alert("ok");$("#city_code").val("");$("#full_name_uz").val("");$("#abb_name_uz").val("");$("#url_site").val("");}
@@ -38,7 +38,7 @@ function foruz(){
 
 function forcity(){
   var city = $('#city_name').val();
-  $.post("http://ravengo.ru/forspec/addercity.php", {city: city} )
+  $.post("addercity.php", {city: city} )
     .done(function(data) {
         if(Number(data)==200){
                 alert("ok");
@@ -53,7 +53,7 @@ function forcity(){
 
 function forregexp(){
   var regexper = $('#regular_exp').val();
-  $.post("http://ravengo.ru/forspec/regexper.php", {regexper: regexper} )
+  $.post("regexper.php", {regexper: regexper} )
     .done(function(data) {
         if(Number(data)==200){
                 alert("ok");}
