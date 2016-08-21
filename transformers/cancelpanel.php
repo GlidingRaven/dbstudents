e50857eaa7ac96dea0d411c2c8d7f27e8355488a
Dunaevsky <admin@ravengo.ru>
2016-08-21 16:41:11 +0600

    Last major update
    added loginbox.php 21/08/2016

--- transformers/cancelpanel.php
+++ transformers/cancelpanel.php
@@ -2,12 +2,7 @@
 
     require $_SERVER['DOCUMENT_ROOT'].'/config.php';
 
-    ////Единый блок авторизации
-    $password = preg_replace("/[^A-Za-z\d]/u", "", $_COOKIE["pass"]); // Берём куки и фильтруем
-    $hash1 = md5($password);            // Хешируем
-    $salt = $config_salt;               // Соль
-    $saltedHash = md5($hash1 . $salt);  // Складываем старый хеш с солью и пропускаем через функцию md5()
-    if ($saltedHash <> '3a38907753c8f7340a8b4bcaf9490ce7') {  echo "Access Denited.";exit();  }
+    require $_SERVER['DOCUMENT_ROOT'].'/loginbox.php';
 
     echo
   '<!DOCTYPE html>
